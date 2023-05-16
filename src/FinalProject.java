f7b38dddc77daabdd4897d4d2cbb1d109054f659
Elambsmith3921 <elambsmith3921@student.pps.net>
2023-05-16 12:50:13 -0700

    started setting up grid

--- src/FinalProject.java
+++ src/FinalProject.java
@@ -1,3 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 public class FinalProject {
+    JFrame window = new JFrame("Color Grid");
+    JButton blueButton = new JButton("Blue");
+    JButton greenButton = new JButton("Green");
+    //JButton[][] board = new JButton[1][1];
+    ArrayList<JButton> board = new ArrayList<>();
+
+    public static void main(String[] args) {
+        new FinalProject();
+    }
 
 }
