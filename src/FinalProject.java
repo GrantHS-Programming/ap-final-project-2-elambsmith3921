f68fe5fd27dce030992856a3ff4a1f9b8600100e
emmalambsmith <elambsmith3921@student.pps.net>
2023-05-18 14:01:19 -0700

    working on guess button function

--- src/FinalProject.java
+++ src/FinalProject.java
@@ -5,12 +5,16 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class FinalProject implements ActionListener {
+
+    int x = 1;
+    int y = 1;
+
     JFrame window = new JFrame("Color Grid");
-    JButton guess = new JButton("Click the color you think the grey");
+    JButton guess = new JButton("Click the color you think the grey square is");
     JButton blueButton = new JButton("Blue");
     JButton greenButton = new JButton("Green");
-    //JButton[][] board = new JButton[1][1];
-    ArrayList<JButton> board = new ArrayList<>();
+    JButton[][] grid = new JButton[x][y];
+
 
     public static void main(String[] args) {
         new FinalProject();
@@ -21,9 +25,11 @@ public class FinalProject implements ActionListener {
         window.setSize(800, 800);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        Container guessContainer = new Container();
         Container blueContainer = new Container();
         Container greenContainer = new Container();
         Container boardContainer = new Container();
+        guessContainer.setLayout(new GridLayout(1,1));
         blueContainer.setLayout(new GridLayout(1, 1));
         greenContainer.setLayout(new GridLayout(1, 1));
         boardContainer.setLayout(new GridLayout(20, 20));
@@ -33,31 +39,46 @@ public class FinalProject implements ActionListener {
         greenButton.addActionListener(this);
         greenContainer.add(greenButton);
 
-        for (int row = 0; row < board.length; row++) {
-            for (int col = 0; col < board[0].length; col++) {
-                board[row][col] = new JButton();
-                board[row][col].addActionListener(this);
-                boardContainer.add(board[row][col]);
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                grid[row][col] = new JButton();
+                grid[row][col].addActionListener(this);
+                boardContainer.add(grid[row][col]);
             }
         }
 
 
         window.add(blueContainer, BorderLayout.NORTH);
-        window.add(greenContainer, BorderLayout.NORTH);
-        window.add(boardContainer, BorderLayout.CENTER);
+        window.add(guessContainer, BorderLayout.NORTH);
+        window.add(greenContainer, BorderLayout.CENTER);
+        window.add(boardContainer, BorderLayout.SOUTH);
 
         window.setVisible(true);
-    }
-
-    public void guess(){
+        getColor();
 
     }
-    public void expansion(){
-        if()
+
+    public String getColor(){
+        String color = "yetToBeSet";
+        int numColor = (int)(Math.random()*grid.length);
+        if(numColor % 2 == 0){
+            color = "blue";
+        }
+        if((numColor % 2 != 0)){
+            color = "green";
+        }
+        return color;
     }
+
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if(e.getSource().equals(blueButton) && getColor().equals("blue")){
+            boolean guessCheck = true;
+            x = x*2;
+            y=  y*2;
+         }
     }
+
 }
