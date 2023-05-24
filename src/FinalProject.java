8a2065dcd70ff3a92fbcefd1a7a826c181e22130
Elambsmith3921 <elambsmith3921@student.pps.net>
2023-05-24 13:48:23 -0700

    replaced and fixed a lot of code that wasn't working/up to date with my newer ideas

--- src/FinalProject.java
+++ src/FinalProject.java
@@ -6,14 +6,14 @@ import java.util.ArrayList;
 
 public class FinalProject implements ActionListener {
 
-    int x = 1;
-    int y = 1;
+    int x = 2;
+    int y = 2;
+
 
     JFrame window = new JFrame("Color Grid");
-    JLabel guess = new JLabel("Click the color you think the grey square is");
-    JButton blueButton = new JButton("Blue");
-    JButton greenButton = new JButton("Green");
-    JButton[][] grid = new JButton[x][y];
+    JButton[][] grid = new JButton[1][1];
+    JButton[][] colorButtons = new JButton[x][y];
+    String[][] gridColor = new String[x][y];
 
 
     public static void main(String[] args) {
@@ -25,19 +25,21 @@ public class FinalProject implements ActionListener {
         window.setSize(800, 800);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        Container colorButtonsContainer = new Container();
         Container guessContainer = new Container();
-        Container blueContainer = new Container();
-        Container greenContainer = new Container();
         Container boardContainer = new Container();
+        colorButtonsContainer.setLayout(new GridLayout(2,1));
         guessContainer.setLayout(new GridLayout(1,1));
-        blueContainer.setLayout(new GridLayout(1, 1));
-        greenContainer.setLayout(new GridLayout(1, 1));
         boardContainer.setLayout(new GridLayout(20, 20));
 
-        blueButton.addActionListener(this);
-        blueContainer.add(blueButton);
-        greenButton.addActionListener(this);
-        greenContainer.add(greenButton);
+        JButton blue = colorButtons[0][0] = new JButton("blue");
+        blue.setBackground(Color.blue);
+        colorButtons[0][0].addActionListener(this);
+        JButton green = colorButtons[1][0] = new JButton("green");
+        green.setBackground(Color.green);
+        colorButtons[1][0].addActionListener(this);
+        guessContainer.add(blue);
+        guessContainer.add(green);
 
         for (int row = 0; row < grid.length; row++) {
             for (int col = 0; col < grid[0].length; col++) {
@@ -46,20 +48,20 @@ public class FinalProject implements ActionListener {
                 boardContainer.add(grid[row][col]);
             }
         }
+
 //maybe make the color guesses a grid of 2, formatting weird here
 
-        window.add(blueContainer, BorderLayout.NORTH);
         window.add(guessContainer, BorderLayout.NORTH);
-        window.add(greenContainer, BorderLayout.CENTER);
         window.add(boardContainer, BorderLayout.SOUTH);
 
         window.setVisible(true);
         instructions();
         getColor();
+        gridColor[0][0] = getColor();
     }
 
     public void instructions(){
-        JOptionPane.showMessageDialog(null, "The grey box is either green or blue. Click either the green or blue button depending on which color you think it is. If you guess right the boxes will double. If you guess wrong you start over.");
+        JOptionPane.showMessageDialog(null, "Welcome to this fun game of pure luck! The grey box is either green or blue to start. Click either the green or blue button depending on which color you think it is. If you guess right the color options will double. If you guess wrong you start over.");
     }
 
     public String getColor(){
@@ -79,27 +81,31 @@ public class FinalProject implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         boolean guessCheck = false;
-        if(e.getSource().equals(blueButton) && getColor().equals("blue")){
+        if(e.getSource().equals(colorButtons[0][0]) && gridColor[0][0].equals("blue")){
             guessCheck = true;
          }
-        if(e.getSource().equals(greenButton) && getColor().equals("green")){
+        if(e.getSource().equals(colorButtons[1][0]) && gridColor[0][0].equals("green")){
             guessCheck = true;
         }
-        if(e.getSource().equals(blueButton) && !getColor().equals("blue")){
+        if(e.getSource().equals(colorButtons[0][0]) && !gridColor[0][0].equals("blue")){
             guessCheck = false;
         }
-        if(e.getSource().equals(greenButton) && !getColor().equals("green")){
+        if(e.getSource().equals(colorButtons[1][0]) && !gridColor[0][0].equals("green")){
             guessCheck = false;
         }
         if(guessCheck == true){
-            JOptionPane.showMessageDialog(null, "Congratulations! You guessed right! Can you keep your streak with a board twice as big?");
+            JOptionPane.showMessageDialog(null, "Congratulations! You guessed right! Can you keep your streak with twice as many color options?");
             x= x*2;
             y= y*2;
+            //clearBoard();
+            //reset: random color, doubled color
         }
         if(guessCheck == false){
             JOptionPane.showMessageDialog(null, "Sorry, you guessed wrong.");
             x=1;
             y=1;
+            //clearBoard();
+            //start over
         }
 
 
